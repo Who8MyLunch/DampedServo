7a3bc8859b7528326e89e2451a886779457047f3
Pierre V. Villeneuve <pierre.villeneuve@gmail.com>
2013-04-09 22:57:07 -0700

    work

--- lego.py
+++ lego.py
@@ -3,6 +3,7 @@ import os
 import time
 
 import numpy as np
+import RPIO
 
 import damped_servo
 
@@ -21,51 +22,41 @@ def test_position(c0, c1):
     # Done.
 
 
-limit_table = [[0.00, 0.00, 0.14],
-               [0.10, 0.00, 0.11],
-               [0.20, 0.00, 0.10],
-               [0.30, 0.00, 0.10],
-               [0.40, 0.10, 0.60],
-               [0.50, 0.20, 0.65],
-               [0.52, 0.30, 0.70],
-               [0.55, 0.40, 0.75],
-               [0.57, 0.50, 0.80],
-               [0.60, 0.50, 0.90],
-               [0.65, 0.50, 1.00],
-               [0.70, 0.55, 1.00],
-               [1.00, 0.60, 1.00],
-               [1.01, 0.60, 1.00]]
-
-limit_table = np.asarray(limit_table)
-
-def dynamic_limits(y_work, y_ref):
-    """
-    Modify servo controls based on external constraints.
-    """
-    edges_L = limit_table[:-1, 0]
-    edges_R = limit_table[1:, 0]
-
-    frac_list = (y_ref - edges_L) / (edges_R - edges_L)
-
-    whr = np.where(np.logical_and(0 <= frac_list, frac_list < 1))
-    row = limit_table[whr][0]
-    y_lo, y_hi = row[1:]
-
-    y_work_mod = y_lo + (y_hi - y_lo) * y_work
-    
-    return y_work_mod
-
-
-
-def make_limiter(D_ref):
-    dl = dynamic_limits
-    def limiter(y_work):
-        y_ref = D_ref.response.y_now
-        y_work_mod = dl(y_work, y_ref)
-        return y_work_mod
-
-    # Done.
-    return limiter
+# limit_table = [[0.00, 0.00, 0.14],
+#                [0.10, 0.00, 0.11],
+#                [0.20, 0.00, 0.10],
+#                [0.30, 0.00, 0.10],
+#                [0.40, 0.10, 0.60],
+#                [0.50, 0.20, 0.65],
+#                [0.52, 0.30, 0.70],
+#                [0.55, 0.40, 0.75],
+#                [0.57, 0.50, 0.80],
+#                [0.60, 0.50, 0.90],
+#                [0.65, 0.50, 1.00],
+#                [0.70, 0.55, 1.00],
+#                [1.00, 0.60, 1.00],
+#                [1.01, 0.60, 1.00]]
+# limit_table = np.asarray(limit_table)
+# def dynamic_limits(y_work, y_ref):
+#     """
+#     Modify servo controls based on external constraints.
+#     """
+#     edges_L = limit_table[:-1, 0]
+#     edges_R = limit_table[1:, 0]
+#     frac_list = (y_ref - edges_L) / (edges_R - edges_L)
+#     whr = np.where(np.logical_and(0 <= frac_list, frac_list < 1))
+#     row = limit_table[whr][0]
+#     y_lo, y_hi = row[1:]
+#     y_work_mod = y_lo + (y_hi - y_lo) * y_work
+#     return y_work_mod
+# def make_limiter(D_ref):
+#     dl = dynamic_limits
+#     def limiter(y_work):
+#         y_ref = D_ref.response.y_now
+#         y_work_mod = dl(y_work, y_ref)
+#         return y_work_mod
+#     # Done.
+#     return limiter
 
     
     
@@ -76,6 +67,8 @@ if __name__ == '__main__':
 
     ###################################
     # Setup.
+    gpio_button = 24
+    
     channel_0 = 3
     channel_1 = 7
     
@@ -91,11 +84,11 @@ if __name__ == '__main__':
     D_0.start()
     D_1.start()
 
-    D_0.scale = 0.1
-    D_0.pulse(1.0)
-    time.sleep(0.5)
+    D_0.scale = 0.5
+    D_0.pulse(0.75)
+    time.sleep(1.)
 
-    D_1.scale = 0.1
+    D_1.scale = 0.2
     D_1.pulse(1.0)
 
     time.sleep(1.0)
@@ -118,7 +111,7 @@ if __name__ == '__main__':
             if i == 0:
                 p = np.random.uniform(0.40, 1.0)
             elif i == 1:
-                p = np.random.uniform(0.20, 1.0)
+                p = np.random.uniform(0.00, 1.0)
                     
             D = D_01[i]
             D.pulse(p)
@@ -128,19 +121,45 @@ if __name__ == '__main__':
             flag_loop = False
     
     # Finish,
-    print('Move back to position zero...')
-    D_0.scale = 0.2
-    D_1.scale = 0.1
+    print('Salute...')
     
-    D_1.pulse(0)
-    time.sleep(0.75)
-    D_0.pulse(0)
-    time.sleep(2.)
+    D_0.pulse(0.75)
+
+    D_1.scale = 0.1
+    D_1.pulse(0.0)
+    time.sleep(0.3)
+    
+    D_0.scale = 0.2
+    D_0.pulse(0.7)
+    time.sleep(1.)
+
+    D_1.pulse(1.0)
+    time.sleep(0.5)
+
+    D_0.pulse(0.2)
+    time.sleep(0.1)
+
+    D_1.pulse(0.8)
+    time.sleep(0.2)
+    D_1.pulse(1.0)
+
+    D_0.scale = 0.5
+    D_0.pulse(0.7)
+    time.sleep(1.0)
+    
+    D_1.pulse(0.0)
+    time.sleep(0.5)
+    
+    print('Move back to position zero...')
+    D_1.scale = 0.1
+    D_1.pulse(0.0)
 
     D_0.scale = 0.1
-    D_0.pulse(0.4)
+    D_0.pulse(0.0)
 
     time.sleep(0.5)
+
+    D_0.pulse(0.0)
     
     print('Shutting down...')
     D_0.stop()
