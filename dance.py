430dd78657ac9a438ec6bf46eb9924835e8735a3
Pierre V. Villeneuve <pierre.villeneuve@gmail.com>
2013-04-16 17:59:05 -0700

    incorporate dancing

---
+++ dance.py
@@ -0,0 +1,22 @@
+
+import damped_servo
+import lego
+import beats
+
+if __name__ == '__main__':
+    fname = 'Manic Polka'
+
+
+
+
+    # Do it.
+    fname_mp3 = fname + '.mp3'
+    fname_wav = fname + '.wav'
+
+    points = beats.process_segments(fname_mp3)
+
+
+
+    # Done.
+    
+    
