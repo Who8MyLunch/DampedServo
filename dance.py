925f230b8bc9e1e7aae1859a17cf01510dbbb27b
Pierre V. Villeneuve <pierre.villeneuve@gmail.com>
2013-04-16 19:52:19 -0700

    work

--- dance.py
+++ dance.py
@@ -1,21 +1,66 @@
 
+import os
+import time
+import ossaudiodev
+import wave
+
 import damped_servo
 import lego
 import beats
 
 if __name__ == '__main__':
+
+    #########################################
+    # Setup.
+    path_data = os.path.abspath(os.path.curdir)
     fname = 'Manic Polka'
 
-
-
-
+    ##########################################
     # Do it.
     fname_mp3 = fname + '.mp3'
     fname_wav = fname + '.wav'
 
-    points = beats.process_segments(fname_mp3)
+    #audio_file = wave.open(fname_wav, 'rb')
+    #params = audio_file.getparams()
+    #num_channels, sample_width, frame_rate, num_frames, comptype, compname = params
+    #data_audio = audio_file.readframes(num_frames)
+    #audio_file.close()
 
+    f = os.path.join(path_data, fname_wav)
+    data_audio, sample_rate = beats.read_wav(f)
+    num_frames, num_channels = data_audio.shape
 
+    # Play back through speaker.
+    audio_device = ossaudiodev.open('w')
+
+    audio_device.setparameters(ossaudiodev.AFMT_S16_LE, num_channels, sample_rate)
+    print(audio_device.bufsize())
+    audio_device.setparameters(ossaudiodev.AFMT_S16_LE, num_channels, sample_rate)
+    print(audio_device.obuffree())
+    time.sleep(0.2)
+
+    audio_device.setparameters(ossaudiodev.AFMT_S16_LE, num_channels, sample_rate)
+    chunk_size = 22050
+    k0 = 0
+    k1 = 0
+    try:
+        while True:
+            print(k0, k1)
+
+            k0 = k1
+            k1 = k0 + chunk_size
+            data_chunk = data_audio[k0:k1]
+
+            t0 = time.time()
+            audio_device.write(data_chunk.tostring())
+            t1 = time.time()
+            print(t1 - t0)
+    except KeyboardInterrupt:
+        audio_device.close()
+        
+
+    # Sound analysis.
+    # points = beats.process_segments(fname_mp3)
 
     # Done.
     
