8cb85fb3a7cc5f09f3c85d9d733b2dc358a9a4c6
Pierre V.Villeneuve <pierre.villeneuve@gmail.com>
2013-04-20 16:13:52 -0700

    work

--- beats.py
+++ beats.py
@@ -1,5 +1,8 @@
 
 import os
+import threading
+import time
+import ossaudiodev
 
 import numpy as np
 import scipy as sp
@@ -20,7 +23,6 @@ def read_wav(fname):
     """
     sample_rate, data = sp.io.wavfile.read(fname)
 
-    # Done.
     return data, sample_rate
 
 
@@ -39,18 +41,21 @@ def echo_nest_analysis(fname_song, fname_config=None):
     Get track details via Echo Nest API.
     """
     if not fname_config:
-        fname_config = 'config.yml'
+        fname_config = 'audio_config.yml'
       
     fname_config = os.path.abspath(fname_config)  
     path_work = os.path.dirname(fname_config)
     
-    path_analysis = os.path.join(path_work, 'Analysis')
+    path_analysis = os.path.join(path_work, 'Audio Analysis')
     if not os.path.isdir(path_analysis):
         os.mkdir(path_analysis)
 
     fname_song = os.path.basename(fname_song)
+    b, e = os.path.splitext(fname_song)
+    if e != '.mp3':
+        fname_song = b + '.mp3'
     
-    fname_analysis = os.path.splitext(fname_song)[0] + '.full.yml'
+    fname_analysis = b + '.full.yml'
 
     f = os.path.join(path_analysis, fname_analysis)
     if os.path.isfile(f):
@@ -87,7 +92,6 @@ def echo_nest_analysis(fname_song, fname_config=None):
         r = requests.get(track.analysis_url)
         analysis = r.json()
 
-    
         print('Save analysis to cache folder')
         f = os.path.join(path_analysis, fname_analysis)
         io.write(f, analysis)
@@ -104,8 +108,7 @@ def parse_segments(analysis):
     points = []
     for segment in analysis['segments']:
         t0 = segment['start']
-        v0 = segment['loudness_start']
-    
+        v0 = segment['loudness_start']        
         p0 = (10.**(v0/20))
         
         t1 = t0 + segment['loudness_max_time']
@@ -128,20 +131,22 @@ def process_segments(fname_song):
     """
 
     path_work = os.path.dirname(fname_song)
-    path_analysis = os.path.join(path_work, 'Analysis')
+    path_analysis = os.path.join(path_work, 'Audio Analysis')
     if not os.path.isdir(path_analysis):
         os.mkdir(path_analysis)
 
-    fname_points = os.path.basename(fname_song) + '.npz'
+    b, e = os.path.splitext(os.path.basename(fname_song))
+    fname_points = b + '.npz'
+    
     f = os.path.join(path_analysis, fname_points)
     if os.path.isfile(f):
-        print('Loading points')
+        print('Loading audio points')
         points, meta = io.read(f)
     else:
-        print('Compute points')
+        print('Analyze song...')
         analysis = echo_nest_analysis(fname_song)
 
-        print('Caching points')
+        print('Caching audio points')
         points = parse_segments(analysis)
 
         io.write(f, points)
@@ -149,7 +154,160 @@ def process_segments(fname_song):
     # Done,
     return points
 
+#############################################################
+
+
+class Player(threading.Thread):
+    """
+    This class handles audio play back with ability to report back about beats.
+    """
     
+    def __init__(self, fname, time_interval=None):
+        """
+        Initialize class.
+        """
+        threading.Thread.__init__(self)
+
+        if not time_interval:
+            time_interval = 0.05
+
+        self.time_interval = time_interval
+
+        self.lock = threading.Lock()
+        self.is_running = False
+
+        self._timestamp = None
+        
+        fname = os.path.normpath(fname)
+
+        if not os.path.isfile(fname):
+            raise IOError('Input file does not exist: %s' % fname)
+        
+        print('Load audio data: %s' % os.path.basename(fname))
+        b, e = os.path.splitext(fname)
+        fname_wav = b + '.wav'
+        data_audio, sample_rate = read_wav(fname_wav)
+
+        num_frames, num_channels = data_audio.shape
+        
+        self.fname = fname
+        self.data_audio = data_audio
+
+        self.num_frames = num_frames
+        self.num_channels = num_channels
+        self.sample_rate = sample_rate
+
+        print('Load audio analysis')
+        self.audio_points = process_segments(fname_song)
+
+        print('Configure audio device')
+        self.chunk_size = int(self.time_interval * self.sample_rate)
+        print('  time interval: %.1f ms' % self.time_interval*1000)
+        print('  audio chunk size: %d' % self.chunk_size)
+        
+        self.audio_device = ossaudiodev.open('w')
+        self.audio_device.setparameters(ossaudiodev.AFMT_S16_LE, self.num_channels, self.sample_rate)
+        print('  audio buffer size: %d' % self.audio_device.bufsize())
+    
+        time.sleep(0.01)
+        self.audio_device.setparameters(ossaudiodev.AFMT_S16_LE, self.num_channels, self.sample_rate)
+
+        # Done.
+        
+        
+        
+    @property
+    def timestamp(self):
+        """
+        Timestamp for use during music playback.
+        """
+        self.lock.acquire()
+        value = self._timestamp
+        self.lock.release()
+        
+        return value
+        
+        
+        
+    @timestamp.setter
+    def timestamp(self, value=None):
+        if value is None:
+            value = time.time()
             
+        self.lock.acquire()
+        self._timestamp = value
+        self.lock.release()
+        
+        
+        
+    def run(self):
+        """
+        This is where the action happens.
+        """
+        k0 = 0
+        k1 = 0
+
+        self.is_running = True
+        try:
+            while self.is_running:
+                
+                k0 = k1
+                k1 = k0 + self.chunk_size
+                data_chunk_str = self.data_audio[k0:k1].tostring()
+    
+                t0 = time.time()
+                self.audio_device.write(data_chunk_str)
+                t1 = time.time()
+                print('%.5f, %.5f' % (t1 - t0, delta))
+    
+        except KeyboardInterrupt:
+            print('\nUser stop!')
+
+        # Finish.
+        self.audio_device.close()
+        
+        # Done.
+
+
+    def stop(self):
+        if self.isAlive():
+            print('Player stopping: %s' % os.path.basename(self.fname))
+
+        self.is_running = False
+
+
+    def beats(self):
+        """
+        Setup a generator to yield beat information in a timely manner.
+        """
+        while self.is_running:
+            yield 'hello!'
+
+
 if __name__ == '__main__':
-    pass
+
+    
+    #########################################
+    # Setup.
+    path_data = os.path.abspath(os.path.curdir)
+    fname = 'Manic Polka'
+
+    ##########################################
+    # Do it.
+    fname_mp3 = fname + '.mp3'
+    fname_wav = fname + '.wav'
+
+
+    try:
+        while True:
+
+    except KeyboardInterrupt:
+        audio_device.close()
+        
+
+    # Sound analysis.
+    # points = beats.process_segments(fname_mp3)
+
+    # Done.
+    
+    
