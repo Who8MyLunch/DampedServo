52a78542d3b17283b967c79db6e71c4ee1e92fb0
Pierre V.Villeneuve <pierre.villeneuve@gmail.com>
2013-04-21 11:49:20 -0700

    Worked on thre LEDs.

--- lego.py
+++ lego.py
@@ -19,46 +19,67 @@ class Controller(object):
         self.scale_1 = scale_1
         self.D_1 = None
 
-        self.pin_red = 23
-        self.pin_green = 25
+        self.pin_butt_red = 23
+        self.pin_butt_yel = 24
+        self.pin_butt_grn = 25
 
-        self.pin_mosfet = 17
-        self.pin_led = 22
+        self.pin_led_red = 4
+        self.pin_led_yel = 17
+        self.pin_led_grn = 21
 
         # Done.
 
 
     def blink_led(self, pin, value):
-        on_off = RPIO.input(self.pin_led)
-        RPIO.output(self.pin_led, not on_off)
+        print('Blink LED')
+
+        RPIO.output(self.pin_led_red, False)
+        RPIO.output(self.pin_led_yel, False)
+        RPIO.output(self.pin_led_grn, False)
+
+        RPIO.output(self.pin_led_red, True)
+        time.sleep(0.1)
+        RPIO.output(self.pin_led_yel, True)
+        time.sleep(0.1)
+        RPIO.output(self.pin_led_grn, True)
         
-            
+        time.sleep(0.5)
+
+        RPIO.output(self.pin_led_red, False)
+        time.sleep(0.1)
+        RPIO.output(self.pin_led_yel, False)
+        time.sleep(0.1)
+        RPIO.output(self.pin_led_grn, False)
+    
+        # Done.
+        
+
     def callback_end_looping(self, pin, value):
-        print('Button: stop main loop!')
+        print('Button: %s, %s.  Stop main loop!' % (pin, value) )
         self.keep_running = False
         
         
     def turn_on(self):
         print('Initialize GPIO pins')
-        RPIO.setup(self.pin_mosfet, RPIO.OUT)
-        RPIO.setup(self.pin_led, RPIO.OUT)
+        RPIO.setup(self.pin_led_red, RPIO.OUT)
+        RPIO.setup(self.pin_led_yel, RPIO.OUT)
+        RPIO.setup(self.pin_led_grn, RPIO.OUT)
 
-        RPIO.setup(self.pin_red, RPIO.IN)
-        RPIO.setup(self.pin_green, RPIO.IN)
-
-        print('Enable servo controller board.')
-        RPIO.output(self.pin_mosfet, True)
-        time.sleep(0.1)
-        
+        RPIO.setup(self.pin_butt_red, RPIO.IN)
+        RPIO.setup(self.pin_butt_yel, RPIO.IN)
+        RPIO.setup(self.pin_butt_grn, RPIO.IN)
         
         print('Configure GPIO callback event handlers')
         fn = self.callback_end_looping
-
-        RPIO.add_interrupt_callback(self.pin_red, fn, edge='falling', pull_up_down=RPIO.PUD_UP,
+        RPIO.add_interrupt_callback(self.pin_butt_red, fn, edge='falling', pull_up_down=RPIO.PUD_UP,
                                     threaded_callback=True, debounce_timeout_ms=100)
 
         fn = self.blink_led
-        RPIO.add_interrupt_callback(self.pin_green, fn, edge='falling', pull_up_down=RPIO.PUD_UP,
+        RPIO.add_interrupt_callback(self.pin_butt_red, fn, edge='falling', pull_up_down=RPIO.PUD_UP,
+                                    threaded_callback=True, debounce_timeout_ms=100)
+        RPIO.add_interrupt_callback(self.pin_butt_yel, fn, edge='falling', pull_up_down=RPIO.PUD_UP,
+                                    threaded_callback=True, debounce_timeout_ms=100)
+        RPIO.add_interrupt_callback(self.pin_butt_grn, fn, edge='falling', pull_up_down=RPIO.PUD_UP,
                                     threaded_callback=True, debounce_timeout_ms=100)
 
         RPIO.wait_for_interrupts(threaded=True)
@@ -181,8 +202,6 @@ class Controller(object):
         self.D_0.stop()
         self.D_1.stop()
 
-        print('Power down servo controller board')
-        RPIO.output(self.pin_mosfet, False)
 
         RPIO.cleanup()
 
