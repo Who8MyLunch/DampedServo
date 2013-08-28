c31076b81b59721f8cb8b784217a1e3e0ffd7cbe
Pierre V. Villeneuve <pierre.villeneuve@gmail.com>
2013-08-27 19:23:55 -0700

    Add some documentation about why this files exists.

--- controls.py
+++ controls.py
@@ -1,5 +1,13 @@
 
-# Testing ideas about responding to user input.
+"""
+Test a few ideas about responding to user input in my Raspberry Pi.
+
+For this example a couple of LEDs and buttons are wired up.
+I'm using the cool RPIO feature of setting up a callback function that
+gets called when I do something like push a button, or release a button.
+
+"""
+
 import time
 import RPIO
 
@@ -14,7 +22,7 @@ info = {'keep_looping': True}
 def button_red(gpio_id, val):
     print("red %s: %s" % (gpio_id, val))
     info['keep_looping'] = False
-    
+
 def button_green(gpio_id, val):
     print("green %s: %s" % (gpio_id, val))
 
@@ -29,9 +37,9 @@ RPIO.add_interrupt_callback(ix_button_green, button_green, edge='falling', pull_
 # Enter main event loop.
 RPIO.wait_for_interrupts(threaded=True)
 
-while info['keep_looping']: 
+while info['keep_looping']:
     time.sleep(0.01)
-    
+
 
 # Finish,
 print('\nExit loop!')
